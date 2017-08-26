c3cfbd3274e2e7173296ddba1beeccd46a561bef
Brandon Huber <brandon.e.huber@gmail.com>
2017-08-26 11:18:27 -0500

    Make compatible with Python 3
    Also, fix downloading latest zip file and convert tabs to spaces

--- auto-updater.py
+++ auto-updater.py
@@ -3,7 +3,6 @@ import os
 import sys
 import zipfile
 import shutil
-import pdb
 
 #Globals
 
@@ -29,81 +28,81 @@ def copytree(src, dst, symlinks=False, ignore=None):
 
 #Update check---------------------------------------------------------------------------
 
-print "Checking for update..."
+print('Checking for update...')
 try:
-	release = requests.get('https://api.github.com/repos/outsetini/Air-for-Steam/releases/latest')
+    latest_release_info = requests.get('https://api.github.com/repos/outsetini/Air-for-Steam/releases/latest')
 except(Exception) as exception:
-	print exception
-	sys.exit(1)
+    print(exception)
+    sys.exit(1)
 
 #For log file write at EOF
-updatetag = release.json()['tag_name']
+updatetag = latest_release_info.json()['tag_name']
 
 if os.path.isfile(logfile):
-	try:
-		with open(logfile, 'r+') as updatelog:
-			if updatelog.read() == updatetag:
-				print 'No update found, quiting'
-				sys.exit(0)
-	except IOError:
-		print 'An error occured trying to read the logfile. Please check write permission'
-		sys.exit(1)
+    try:
+        with open(logfile, 'r+') as updatelog:
+            if updatelog.read() == updatetag:
+                print('No update found, quitting')
+                sys.exit(0)
+    except IOError:
+        print('An error occurred trying to read the logfile. Please check write permission.')
+        sys.exit(1)
 
 #Download and extraction ---------------------------------------------------------
 
 #Zip request
-print 'Update found, starting download...'
+print('Update found, starting download...')
 try:
-	release = requests.get('https://api.github.com/repos/outsetini/Air-for-Steam/zipball/2015-1012a')
+    latest_release = requests.get(latest_release_info.json()['zipball_url'])
 except(Exception) as exception:
-	print exception
-	sys.exit(1)
+    print(exception)
+    sys.exit(1)
 
 #Download zip
 try:
-	with open(os.path.join(skinsDir, 'temp.zip'), 'wb') as temp:
-		temp.write(release.content)
+    with open(os.path.join(skinsDir, 'temp.zip'), 'wb') as temp:
+        temp.write(latest_release.content)
 except(Exception) as exception:
-	print 'Error saving skin:', exception
-	sys.exit(1)
+    print('Error saving skin:', exception)
+    sys.exit(1)
 
 #Extract and delete zip
 with zipfile.ZipFile(os.path.join(skinsDir, 'temp.zip')) as temp:
-	skin = temp.infolist()[0]
-	temp.extractall(os.path.join(steamDir, 'skins'))
-	temp.close()
-	os.remove(os.path.join(skinsDir, 'temp.zip'))
-	if os.path.exists(os.path.join(skinsDir, 'Air for steam (Auto-updated)')):
-		shutil.rmtree(os.path.join(skinsDir, 'Air for steam (Auto-updated)')) #Remove old skin to force rename
-	os.rename(os.path.join(skinsDir, skin.filename[:-1]), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
+    skin = temp.infolist()[0]
+    temp.extractall(os.path.join(steamDir, 'skins'))
+    temp.close()
+    os.remove(os.path.join(skinsDir, 'temp.zip'))
+    if os.path.exists(os.path.join(skinsDir, 'Air for steam (Auto-updated)')):
+        shutil.rmtree(os.path.join(skinsDir, 'Air for steam (Auto-updated)')) #Remove old skin to force rename
+    os.rename(os.path.join(skinsDir, skin.filename[:-1]), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
 
-print 'Skin downloaded and extracted. Preparing configuration...'
+print('Skin downloaded and extracted. Preparing configuration...')
 
 #Configuration----------------------------------------------------------------------------
 
 #Copy config file
 if os.path.isfile(skinconfig):
-	shutil.copy2(os.path.join(skinsDir, 'config.ini'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
+    shutil.copy2(os.path.join(skinsDir, 'config.ini'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
 else:
-	print 'No config file found (It should be named config.ini)'
+    print('No config file found (It should be named config.ini)')
 
 #Copy extras
 if os.path.isfile(config):
-	with open(config, 'r') as config:
-		if config.readline() == 'dark':
-			copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Themes', 'Dark'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
-			print 'Dark theme applied'
-		if config.readline() == 'square':
-			copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Square Avatars'), os.path.join(skinsDir, 'Air for steam (Auto-updated)', 'Graphics'))
-			print 'Square avatars applied'
+    with open(config, 'r') as config:
+        if config.readline() == 'dark':
+            copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Themes', 'Dark'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
+            print('Dark theme applied')
+        if config.readline() == 'square':
+            copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Square Avatars'), os.path.join(skinsDir, 'Air for steam (Auto-updated)', 'Graphics'))
+            print('Square avatars applied')
 else:
-	print 'No extras applied'
+    print('No extras applied')
 
 #End------------------------------------------------------------------
 with open(logfile, 'w') as updatelog:
-	try:
-		updatelog.write(updatetag)
-		print 'Update successful'
-	except(IOError):
-		print 'An error occured writing to logfile, quiting...'
-		sys.exit(1)
+    try:
+        updatelog.write(updatetag)
+        print('Update successful')
+    except(IOError):
+        print('An error occurred writing to logfile, quitting...')
+        sys.exit(1)
