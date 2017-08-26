e0e421e044b6c94968415bd9858d203615ee108d
Brandon Huber <brandon.e.huber@gmail.com>
2017-08-26 12:20:55 -0500

    Refactor logic into functions and reduce dependence on globals

--- auto-updater.py
+++ auto-updater.py
@@ -4,15 +4,8 @@ import sys
 import zipfile
 import shutil
 
-#Globals
+_AIR_FOLDER_NAME = 'Air for steam (Auto-updated)'
 
-steamDir = 'C:\Program Files (x86)\Steam'
-skinsDir = os.path.join(steamDir, 'skins')
-logfile = os.path.join(skinsDir, 'auto-updater log.txt')
-config = os.path.join(skinsDir, 'config.txt')
-skinconfig = os.path.join(skinsDir, 'config.ini')
-
-#Def--------------------------------------------------------------------------------------
 
 def copytree(src, dst, symlinks=False, ignore=None):
     if not os.path.exists(dst):
@@ -26,83 +19,92 @@ def copytree(src, dst, symlinks=False, ignore=None):
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
 
-#Update check---------------------------------------------------------------------------
 
-print('Checking for update...')
-try:
-    latest_release_info = requests.get('https://api.github.com/repos/outsetini/Air-for-Steam/releases/latest')
-except(Exception) as exception:
-    print(exception)
-    sys.exit(1)
+def GetInstalledVersion(log_path):
+    if os.path.isfile(log_path):
+        with open(log_path, 'r', encoding='utf-8') as file:
+            installed_version = file.read().strip()
 
-#For log file write at EOF
-updatetag = latest_release_info.json()['tag_name']
+    return installed_version
 
-if os.path.isfile(logfile):
-    try:
-        with open(logfile, 'r+') as updatelog:
-            if updatelog.read() == updatetag:
-                print('No update found, quitting')
-                sys.exit(0)
-    except IOError:
-        print('An error occurred trying to read the logfile. Please check write permission.')
-        sys.exit(1)
 
-#Download and extraction ---------------------------------------------------------
+def GetLatestReleaseInfo():
+    return requests.get('https://api.github.com/repos/outsetini/Air-for-Steam/releases/latest')
 
-#Zip request
-print('Update found, starting download...')
-try:
-    latest_release = requests.get(latest_release_info.json()['zipball_url'])
-except(Exception) as exception:
-    print(exception)
-    sys.exit(1)
 
-#Download zip
-try:
-    with open(os.path.join(skinsDir, 'temp.zip'), 'wb') as temp:
-        temp.write(latest_release.content)
-except(Exception) as exception:
-    print('Error saving skin:', exception)
-    sys.exit(1)
+def DownloadRelease(release_info, skins_dir):
+    release_zip = requests.get(release_info.json()['zipball_url'])
 
-#Extract and delete zip
-with zipfile.ZipFile(os.path.join(skinsDir, 'temp.zip')) as temp:
-    skin = temp.infolist()[0]
-    temp.extractall(os.path.join(steamDir, 'skins'))
-    temp.close()
-    os.remove(os.path.join(skinsDir, 'temp.zip'))
-    if os.path.exists(os.path.join(skinsDir, 'Air for steam (Auto-updated)')):
-        shutil.rmtree(os.path.join(skinsDir, 'Air for steam (Auto-updated)')) #Remove old skin to force rename
-    os.rename(os.path.join(skinsDir, skin.filename[:-1]), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
+    TEMP_ZIP_FILEPATH = os.path.join(skins_dir, 'temp.zip')
+    with open(TEMP_ZIP_FILEPATH, 'wb') as temp:
+        temp.write(release_zip.content)
 
-print('Skin downloaded and extracted. Preparing configuration...')
+    #Extract and delete zip
+    with zipfile.ZipFile(TEMP_ZIP_FILEPATH) as temp:
+        skin = temp.infolist()[0]
+        temp.extractall(skins_dir)
+    os.remove(TEMP_ZIP_FILEPATH)
 
-#Configuration----------------------------------------------------------------------------
+    LOCAL_RELEASE_PATH = os.path.join(skins_dir, _AIR_FOLDER_NAME)
+    if os.path.exists(LOCAL_RELEASE_PATH):
+        shutil.rmtree(LOCAL_RELEASE_PATH) #Remove old skin to force rename
+    os.rename(os.path.join(skins_dir, skin.filename[:-1]), LOCAL_RELEASE_PATH)
 
-#Copy config file
-if os.path.isfile(skinconfig):
-    shutil.copy2(os.path.join(skinsDir, 'config.ini'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
-else:
-    print('No config file found (It should be named config.ini)')
 
-#Copy extras
-if os.path.isfile(config):
-    with open(config, 'r') as config:
-        if config.readline() == 'dark':
-            copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Themes', 'Dark'), os.path.join(skinsDir, 'Air for steam (Auto-updated)'))
-            print('Dark theme applied')
-        if config.readline() == 'square':
-            copytree(os.path.join(skinsDir, 'Air for steam (Auto-updated)','+Extras', 'Square Avatars'), os.path.join(skinsDir, 'Air for steam (Auto-updated)', 'Graphics'))
-            print('Square avatars applied')
-else:
-    print('No extras applied')
+def CopyConfig(skins_dir):
+    SKIN_CONFIG_PATH = os.path.join(skins_dir, 'config.ini')
+    CONFIG_PATH = os.path.join(skins_dir, 'config.txt')
+    AIR_FOLDER_PATH = os.path.join(skins_dir, _AIR_FOLDER_NAME)
 
-#End------------------------------------------------------------------
-with open(logfile, 'w') as updatelog:
-    try:
-        updatelog.write(updatetag)
-        print('Update successful')
-    except(IOError):
-        print('An error occurred writing to logfile, quitting...')
-        sys.exit(1)
+    # Copy config file
+    if os.path.isfile(SKIN_CONFIG_PATH):
+        shutil.copy2(SKIN_CONFIG_PATH, os.path.join(skins_dir, _AIR_FOLDER_NAME))
+    else:
+        print('No config file found (It should be named config.ini)')
+
+    # Copy extras
+    if os.path.isfile(CONFIG_PATH):
+        with open(CONFIG_PATH, 'r') as file:
+            for line in file:
+                if line.strip() == 'dark':
+                    copytree(os.path.join(AIR_FOLDER_PATH, '+Extras', 'Themes', 'Dark'), AIR_FOLDER_PATH)
+                    print('Dark theme applied')
+                if line.strip() == 'square':
+                    copytree(os.path.join(AIR_FOLDER_PATH, '+Extras', 'Square Avatars'), os.path.join(AIR_FOLDER_PATH, 'Graphics'))
+                    print('Square avatars applied')
+    else:
+        print('No extras applied')
+
+
+def UpdateLog(log_path, version):
+    with open(log_path, 'w', encoding='utf-8') as file:
+        file.write(version)
+
+
+def main():
+    STEAM_DIR = 'C:\Program Files (x86)\Steam'  # TODO: Find steam installation location
+    SKINS_DIR = os.path.join(STEAM_DIR, 'skins')
+    LOG_PATH = os.path.join(SKINS_DIR, 'auto-updater log.txt')
+
+    installed_version = GetInstalledVersion(LOG_PATH)
+    print(f'Local version:  {installed_version}')
+
+    latest_release_info = GetLatestReleaseInfo()
+    latest_release_version = latest_release_info.json()['tag_name']
+    print(f'Latest release: {latest_release_version}')
+
+    if installed_version == latest_release_version:
+        print('Air for Steam is already up-to-date')
+    else:
+        print('Downloading update...')
+        DownloadRelease(latest_release_info, SKINS_DIR)
+
+        print('Applying configuration...')
+        CopyConfig(SKINS_DIR)
+
+        UpdateLog(LOG_PATH, latest_release_version)
+        print('Updated successfully. Steam must be restarted for the changes to take effect.')
+
+
+if __name__ == '__main__':
+    main()
